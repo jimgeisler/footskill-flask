52fbd9b820a03efa28c89ae126c3d7d305fa2215
James Geisler <jim.geisler@gmail.com>
2023-05-24 22:17:09 -0400

    Setup a connection to a mongo database using flask_pymongo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # footskill-flask
 Flask web server to provide an online API to the footskill data set.
+
+Deploys to https://footskill-api.onrender.com
--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import os
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
+# Configure MongoDB connection
+username = os.environ.get('username')
+password = os.environ.get('password')
+app.config['MONGO_URI'] = 'mongodb+srv://<username>:<password>@footskillcluster.t2szqp3.mongodb.net/?retryWrites=true&w=majority'
+
+username = os.environ.get('username')
+password = os.environ.get('password')
+database = 'footskill'
+cluster_url = 'footskillcluster.t2szqp3.mongodb.net'
+
+app.config['MONGO_URI'] = f'mongodb+srv://{username}:{password}@{cluster_url}/{database}?retryWrites=true&w=majority'
+
+# Create an instance of PyMongo
+mongo = PyMongo(app)
+
 # Define a route and its corresponding handler function
 @app.route('/')
 def index():
+    # Access MongoDB collection
+    collection = mongo.db.players
+
+    # Perform database operations
+    data = collection.find()
+
     return 'Hello, World!'
 
 # Run the application if this script is executed directly
